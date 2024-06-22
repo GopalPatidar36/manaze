04ade048ab8971e2418b2dec7a414474a3fcdeb8
gopalp <gopalp@wethos.ai>
2024-06-22 14:27:30 +0530

    Revert code

--- backend/www.js
+++ backend/www.js
@@ -1,7 +1,7 @@
-// const environment = require("./src/database/environment");
-// const database = require("./src/database/database");
+const environment = require("./src/database/environment");
+const database = require("./src/database/database");
 (async function () {
-  // await environment();
-  // await database();
+  await environment();
+  await database();
   require("./express");
 })();
