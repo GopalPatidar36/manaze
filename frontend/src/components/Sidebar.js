0bff8cb074822f92d7c86764b196267265718d29
gopalp <gopalp@wethos.ai>
2024-06-26 12:35:49 +0530

    implement responsive header

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -4,19 +4,32 @@ import { FaChalkboardTeacher, FaBoxTissue } from "react-icons/fa";
 import { SiNamecheap } from "react-icons/si";
 
 const Sidebar = () => {
-  return (
-    <div className="sidebar">
-      {/* <NavLink to="/">
+  const navLink = () => {
+    return (
+      <div className="navlink">
+        {/* <NavLink to="/">
         <FaChalkboardTeacher className="sidebarIcon" /> Board
       </NavLink> */}
-      <NavLink to="/">
-        <FaBoxTissue className="sidebarIcon" /> Issue
-      </NavLink>
-      <NavLink to="/backlog">
-        <SiNamecheap className="sidebarIcon" />
-        Backlog
-      </NavLink>
-    </div>
+        <NavLink to="/">
+          <FaBoxTissue className="sidebarIcon" /> Issue
+        </NavLink>
+        <NavLink to="/backlog">
+          <SiNamecheap className="sidebarIcon" />
+          Backlog
+        </NavLink>
+      </div>
+    );
+  };
+  return (
+    <>
+      <div className="sidebar">
+        {navLink()}
+        <button className="sidebarButton">
+          <span className="kababMenu">☰</span>
+          {navLink()}
+        </button>
+      </div>
+    </>
   );
 };
 
