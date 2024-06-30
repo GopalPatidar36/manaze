cbdc9e015f715819c445df56751085070e6aa09e
gopalp <gopalp@wethos.ai>
2024-06-30 14:10:26 +0530

    revert code

--- backend/www.js
+++ backend/www.js
@@ -1,16 +1,15 @@
 const http = require("http");
 const environment = require("./src/database/environment");
-// const database = require("./src/database/database");
-let server;
+const database = require("./src/database/database");
 (async function () {
   await environment();
-  // await database();
+  await database();
   const expressApp = require("./express");
   const PORT = process.env.PORT || 6036;
-  server = http.createServer(expressApp);
+  const server = http.createServer(expressApp);
   server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
   });
 })();
 
-module.exports = server;
+module.exports = {};
