c703cd187f5f530c572b09354f426fe2d58d38b8
gopalp <gopalp@wethos.ai>
2024-06-30 10:56:07 +0530

    export server in  www.js file

--- backend/www.js
+++ backend/www.js
@@ -1,9 +1,16 @@
+const http = require("http");
 const environment = require("./src/database/environment");
 const database = require("./src/database/database");
+let server;
 (async function () {
   await environment();
   await database();
-  require("./express");
+  const expressApp = require("./express");
+  const PORT = process.env.PORT || 6036;
+  server = http.createServer(expressApp);
+  server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+  });
 })();
 
-module.exports;
+module.exports = server;
