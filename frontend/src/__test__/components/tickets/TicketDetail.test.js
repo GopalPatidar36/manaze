9240c79f5979d9d26e7af89ed4f50cf8b2558fa4
gopalp <gopalp@wethos.ai>
2024-07-31 18:53:06 +0530

    write a test test for text area

--- frontend/src/__test__/components/tickets/TicketDetail.test.js
+++ frontend/src/__test__/components/tickets/TicketDetail.test.js
@@ -3,10 +3,9 @@ import TicketDetail from "../../../components/tickets/TicketDetail";
 import { render } from "../../../testUtils";
 import { useParams } from "react-router-dom";
 
-import { createTicket, updateTicket, getAllTicket, getCurrentUSERTicket, getUpdatedTicket, getTicket } from "../dummyData";
+import { createTicket, getAllTicket, getCurrentUSERTicket, getTicket } from "../dummyData";
 
 const mocks = [getTicket, createTicket, getAllTicket, getCurrentUSERTicket];
-const updateMocks = [getTicket, getAllTicket, getCurrentUSERTicket, getUpdatedTicket, updateTicket];
 
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
@@ -14,11 +13,11 @@ jest.mock("react-router-dom", () => ({
 }));
 
 describe("Ticket details Modal", () => {
-  let headerData;
-  const closeModal = () => {};
+  const closeModal = jest.fn(); // Mock the closeModal function
+  let components;
   beforeEach(async () => {
     useParams.mockReturnValue({ id: "12" });
-    headerData = render(<TicketDetail closeModal={closeModal} id="12" />, { apolloMocks: mocks });
+    components = render(<TicketDetail closeModal={closeModal} id="12" />, { apolloMocks: mocks });
   });
 
   test("Check Modal Heading", async () => {
@@ -42,23 +41,14 @@ describe("Ticket details Modal", () => {
       expect(status[1].value).toBe("OPEN");
     });
   });
+
+  test("Update Ticket", async () => {
+    // Use await with waitFor to handle async operations
+    await waitFor(() => {
+      const title = screen.getByText("dummpy title for test");
+      expect(title).toBeInTheDocument();
+      fireEvent.change(title, { target: { value: "update title for test" } });
+    });
+    expect(screen.getByText(/Save/)).toBeInTheDocument();
+  });
 });
-
-// const closeModal = jest.fn(); // Mock the closeModal function
-
-// test("Update Ticket", async () => {
-//   // Use await with waitFor to handle async operations
-//   await waitFor(() => {
-//     const title = screen.getByPlaceholderText("Enter title");
-//     expect(title).toBeInTheDocument();
-//     expect(title.value).toBe("dummpy title for test");
-//   });
-
-//   const title = screen.getByPlaceholderText("Enter title");
-//   fireEvent.change(title, { target: { value: "update title for test" } });
-//   expect(title.value).toBe("update title for test");
-
-//   await waitFor(() => {
-//     expect(closeModal).toHaveBeenCalled();
-//   });
-// });
